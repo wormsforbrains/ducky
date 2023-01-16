1f60785c5a645bf89b5c001f3a12a3b49a4c0d85
wormsforbrains <k.klassen000@gmail.com>
2023-01-15 19:15:53 -0800

    added anti-aliased analogs to drawing lines

--- src/main/java/dev/kai/ImageDraw.java
+++ src/main/java/dev/kai/ImageDraw.java
@@ -104,4 +104,85 @@ public class ImageDraw {
             }
         }
     }
+
+    /**
+     * Draws an anti-aliased line.
+     *
+     * @param x1    The x-coordinate of the origin point.
+     * @param y1    The y-coordinate of the origin point.
+     * @param x2    The x-coordinate of the destination point.
+     * @param y2    The y-coordinate of the destination point.
+     * @param color The color of the line.
+     */
+    public void lineAA(int x1, int y1, int x2, int y2, int color) {
+        BufferedImage img = image.getBuffer();
+
+        int dx = x2 - x1;
+        int dy = y2 - y1;
+        int steps = Math.max(Math.abs(dx), Math.abs(dy));
+
+        float xIncrement = (float) dx / (float) steps;
+        float yIncrement = (float) dy / (float) steps;
+
+        float x = x1;
+        float y = y1;
+
+        for (int i = 0; i <= steps; i++) {
+            int xInt = Math.round(x);
+            int yInt = Math.round(y);
+
+            // calculate the alpha value based on the distance from the nearest pixel
+            float alpha = 1 - Math.max(Math.abs(x - xInt), Math.abs(y - yInt));
+
+            // blend the color with the existing pixel color
+            Color blendedColor = blendColors(img.getRGB(xInt, yInt), color, alpha);
+
+            img.setRGB(xInt, yInt, blendedColor.getRGB());
+
+            x += xIncrement;
+            y += yIncrement;
+        }
+    }
+
+    /**
+     * Draws anti-aliased lines.
+     *
+     * @param lines An array containing the "lines".
+     * @param color The color of the line.
+     */
+    public void linesAA(Integer[][] lines, int color) {
+        BufferedImage img = image.getBuffer();
+        for (var line : lines) {
+            int x1 = line[0];
+            int y1 = line[1];
+            int x2 = line[2];
+            int y2 = line[3];
+
+            int dx = x2 - x1;
+            int dy = y2 - y1;
+            int steps = Math.max(Math.abs(dx), Math.abs(dy));
+
+            float xIncrement = (float) dx / (float) steps;
+            float yIncrement = (float) dy / (float) steps;
+
+            float x = x1;
+            float y = y1;
+
+            for (int i = 0; i <= steps; i++) {
+                int xInt = Math.round(x);
+                int yInt = Math.round(y);
+
+                // calculate the alpha value based on the distance from the nearest pixel
+                float alpha = 1 - Math.max(Math.abs(x - xInt), Math.abs(y - yInt));
+
+                // blend the color with the existing pixel color
+                Color blendedColor = blendColors(img.getRGB(xInt, yInt), color, alpha);
+
+                img.setRGB(xInt, yInt, blendedColor.getRGB());
+
+                x += xIncrement;
+                y += yIncrement;
+            }
+        }
+    }
 }
